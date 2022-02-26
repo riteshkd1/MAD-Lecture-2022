97f50ccbf5dc3f18be0e6115c1146fca4feb8bc7
ritesh <riteshkd997@gmail.com>
2022-02-26 17:51:17 +0545

    second commit

--- app/src/main/java/com/example/lectures/MainActivity.java
+++ app/src/main/java/com/example/lectures/MainActivity.java
@@ -23,5 +23,7 @@ public class MainActivity extends AppCompatActivity {
 
         Log.d(TAG, "count: "+String.valueOf(count));
         Log.d(TAG, "second count: "+String.valueOf(count));
+
+        Log.d(TAG, "count: "+String.valueOf(count));
     }
 }
