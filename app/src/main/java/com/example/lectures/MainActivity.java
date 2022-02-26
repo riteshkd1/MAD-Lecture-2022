f9484234797c6c222d89ab2dee26908188708fab
ritesh <riteshkd997@gmail.com>
2022-02-26 17:49:12 +0545

    initial commit

---
+++ app/src/main/java/com/example/lectures/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.lectures;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    private int count;
+
+    public static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        count = 10;
+
+        Log.d("MainActivity",""+count);
+        Log.d("MainActivity", String.valueOf(count));
+
+        Log.d(TAG, "count: "+String.valueOf(count));
+        Log.d(TAG, "second count: "+String.valueOf(count));
+    }
+}
